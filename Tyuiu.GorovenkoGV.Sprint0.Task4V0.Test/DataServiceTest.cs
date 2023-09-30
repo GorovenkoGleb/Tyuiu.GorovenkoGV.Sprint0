628f9e35f83c6d9635df30384713403a6d45b799
GorovenkoGleb <gorovenkogv@outlook.com>
2023-09-30 19:21:24 +0500

    Task4

---
+++ Tyuiu.GorovenkoGV.Sprint0.Task4V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.GorovenkoGV.Sprint0.Task4V0.Lib;
+namespace Tyuiu.GorovenkoGV.Sprint0.Task4V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ChackedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void ChackedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void ChackedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void ChackedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
